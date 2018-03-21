5ababc91077d927222dafebdf5719b2adbf0b18b
Walee Ahmed <dev@walee.me>
2018-03-21 01:11:37 -0400

    initial project structure and working nextjs ssr app on firebase function

---
+++ apps/web/client/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Typography from "material-ui/Typography"
+
+const IndexPage = () => (
+  <div>
+    <Typography variant="display1" gutterBottom>
+      Hello WHCS
+    </Typography>
+  </div>
+)
+
+export default IndexPage
